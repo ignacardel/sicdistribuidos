4302ee9be5026ce0665c1485e929fd74aa22ee4b
venezuelanpride23 <venezuelanpride23@c81cbf31-4332-6fb2-1278-fe36cbbeadfc>
2011-05-08 01:17:42 +0000

    Prueba Subida Servidor

---
+++ trunk/Servidor/src/servidor/HiloEnvio.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ignaciocardenas
+ */
+public class HiloEnvio implements Runnable {
+
+    private FileInputStream archivo;
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+    private String ruta = "Archivos/";
+    private String nombrearchivo;
+    private String solicitud;
+    private Socket s;
+    private long cantidaddescargada;
+
+    HiloEnvio(Socket nuevosocketcliente) {
+        try {
+            s = nuevosocketcliente;
+            entrada = new DataInputStream(s.getInputStream());
+            salida = new DataOutputStream(s.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(HiloEnvio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void run() {
+        try {
+            solicitud = entrada.readUTF();
+            if (solicitud.equals("GET")) {
+                this.enviararchivo();
+            }
+            if (solicitud.equals("RDIR")) {
+                this.listararhivos();
+            }
+            s.close();
+            System.out.println("Fin de conexion de " + s.getInetAddress());
+        } catch (IOException ex) {
+            Logger.getLogger(HiloEnvio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void enviararchivo() {
+        try {
+            nombrearchivo = entrada.readUTF();
+            archivo = new FileInputStream(ruta + nombrearchivo);
+            cantidaddescargada = entrada.readLong();
+
+            if (cantidaddescargada != 0) {
+                archivo.skip(cantidaddescargada);
+            }
+            System.out.println("Iniciando transmision con" + String.valueOf(archivo.getChannel().size()) + " bytes");
+            byte[] buffer = new byte[1024];
+            int len;
+            int total = 0;
+            while ((len = archivo.read(buffer)) > 0) {
+                salida.write(buffer, 0, len);
+                total = total + len;
+                System.out.println("Enviando " + String.valueOf(len) + " total " + total);
+            }
+            System.out.println("Fin de envio");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void listararhivos() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ trunk/Servidor/src/servidor/Servidor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author ignaciocardenas
+ */
+public class Servidor {
+
+    public Servidor() {
+        try {
+            ServerSocket socketservidor = new ServerSocket(6000);
+            while (true) {
+                System.out.println("Esperando usuario..");
+                Socket nuevosocketcliente = socketservidor.accept();
+                System.out.println("Se conecto " + nuevosocketcliente.getInetAddress());
+                HiloEnvio nuevocliente = new HiloEnvio(nuevosocketcliente);
+                Thread nuevohilocliente = new Thread(nuevocliente);
+                nuevohilocliente.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
