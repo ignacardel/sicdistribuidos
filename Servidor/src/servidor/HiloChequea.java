6373ffc366580cad2ac1b0be2ee5ae608f3053d2
cotufa9 <cotufa9@c81cbf31-4332-6fb2-1278-fe36cbbeadfc>
2011-05-10 20:03:09 +0000

    verficar caida de servidores

--- Servidor/src/servidor/HiloChequea.java
+++ Servidor/src/servidor/HiloChequea.java
@@ -24,21 +24,45 @@ public class HiloChequea implements Runnable {
     private DataOutputStream salida;
     private Socket s;
     private HiloServidor hiloservidor;
+    private int opcion;
+    private String recibe;
+    private int numerochequeador;
 
-    public HiloChequea(Socket nuevosocketcliente, HiloServidor hiloservidor){
-      try {
-            s = nuevosocketcliente;
-            entrada = new DataInputStream(s.getInputStream());
-            salida = new DataOutputStream(s.getOutputStream());
-
-        } catch (IOException ex) {
-            Logger.getLogger(HiloEnvio.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+    public HiloChequea(DataInputStream entrada, DataOutputStream salida, HiloServidor hiloservidor, int opcion, int numerochequeador){
+      
+            this.entrada = entrada;
+            this.salida = salida;
+            this.opcion = opcion;
+            this.numerochequeador = numerochequeador;
     }
 
     public void run() {
 
+
+        while (true)
+        {
+            if ( opcion == 0)
+            {
+                try {
+                    try {Thread.sleep(2000); } catch (InterruptedException ex) { System.out.println("pausa");}
+                    salida.writeUTF("act");
+                    System.out.println("sigue vivo clien");
+                    recibe = entrada.readUTF();
+                } catch (IOException ex) {
+                    hiloservidor.quitarservidor(this.numerochequeador);
+                    System.out.println("SE CAYO");}
+            }
+            else
+            {
+                try {
+                    recibe = entrada.readUTF();
+                    System.out.println("sigue activo");
+                    salida.writeUTF("act");
+                } catch (IOException ex) {
+                    hiloservidor.quitarservidor(this.numerochequeador);
+                    System.out.println("SE CAYO");}
+            }
+        }
 
     }
 
